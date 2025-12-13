309011eb13836d1c2cc14c03cc357241fd33ae85
MO IMRAN <moimran16218227@gmail.com>
2025-12-13 18:26:01 +0530

    Processed Ai response and saved in database

--- aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -3,8 +3,6 @@ package com.fitness.aiservice.service;
 import com.fitness.aiservice.model.Recommendation;
 import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
